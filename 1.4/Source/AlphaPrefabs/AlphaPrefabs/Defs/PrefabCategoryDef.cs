d980fc3620d57fe16aaf30d23c6377ec79d751da
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-06-21 19:47:40 +0200

    v0.1

--- 1.4/Source/AlphaPrefabs/AlphaPrefabs/Defs/PrefabCategoryDef.cs
+++ 1.4/Source/AlphaPrefabs/AlphaPrefabs/Defs/PrefabCategoryDef.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RimWorld;
+using Verse;
 
-namespace AlphaPrefabs.Defs
+namespace AlphaPrefabs
 {
-    internal class PrefabCategoryDef
+    public class PrefabCategoryDef : Def
     {
+
+        public string icon;
+        public float priority;
+
     }
 }
--- 1.4/Source/AlphaPrefabs/AlphaPrefabs/Defs/PrefabDef.cs
+++ 1.4/Source/AlphaPrefabs/AlphaPrefabs/Defs/PrefabDef.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KCSG;
+using RimWorld;
+using Verse;
 
-namespace AlphaPrefabs.Defs
+namespace AlphaPrefabs
 {
-    internal class PrefabDef
+    public class PrefabDef : Def
     {
+        public string icon;
+        public float priority;
+        public PrefabCategoryDef category;
+        public StructureLayoutDef layout;
+        public float marketvalue;
+        public List<ResearchProjectDef> researchPrerequisites;
     }
 }
